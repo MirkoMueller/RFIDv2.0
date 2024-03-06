88a1313e20682c70aac61b727ff08b7cd7c8b26e
Mirko Mueller <M.mueller@bbw-fi.de>
2024-03-06 15:26:13 +0100

    Version 2.0

---
+++ rfidv2.0.py
@@ -0,0 +1,60 @@
+import serial
+import pymysql
+import datetime
+
+
+# Serielle Verbindung zum Arduino herstellen
+ser = serial.Serial('COM5', 9600)  # 'COM4' an Ihre Umgebung anpassen
+
+# Verbesserte Fehlerbehandlung bei der MySQL-Verbindung
+try:
+    db_rfid_cards = pymysql.connect(host='localhost', user='root', password='', database='rfid_cards')
+    cursor = db_rfid_cards.cursor()
+
+except pymysql.Error as e:
+    print(f"Fehler bei der MySQL-Verbindung: {e}")
+    exit(1)
+
+# Daten in geeignete Datentypen konvertieren
+
+Zeitstempel = str(datetime.datetime.now())  # Zeitstempel als String im aktuellen Format,
+
+
+try:
+    while True:
+        # Daten vom Arduino lesen und leere Zeilen ignorieren
+        line1 = ''
+        line2 = ''
+
+        while not line1:
+            line1 = ser.readline().decode().strip()
+        while not line2:
+            line2 = ser.readline().decode().strip()
+
+        if line1 and line2:
+            try:
+                # SELECT-Abfrage ausführen
+                cursor.execute("""SELECT rfid_uid2 FROM mitarbeiter """)
+                inhalt = cursor.fetchall()
+
+                # Alle Zeilen ausgeben
+                for zeile in inhalt:
+                    print(zeile)
+
+                # SQL-Abfrage mit korrekten Platzhaltern und Escape-Sequenz
+                sql = f"""INSERT INTO rfidcards(Card_detected, rfid_uid1, Zeitstempel) VALUES (%s, %s, %s)"""
+
+                cursor.execute(sql, (line1, line2, Zeitstempel))
+                db_rfid_cards.commit()  # Änderungen committen
+
+                print(
+                    f"RFID-Karte mit Card detected:{line1} und UID: {line2} und Zeitstempel: {Zeitstempel} erfolgreich gespeichert.")
+
+            except (pymysql.Error, ValueError) as e:
+                print(f"Fehler beim Speichern der RFID-Karte: {e}")
+except KeyboardInterrupt:
+    print("Programm beendet.")
+finally:
+    # Verbindung schließen, falls vorhanden
+    if db_rfid_cards:
+        db_rfid_cards.close()
